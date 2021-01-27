2c322c5c18ab20a32b8d49da1e75e6c937e2de04
christianj2k <78098764+christianj2k@users.noreply.github.com>
2021-01-27 11:47:36 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***********************************************************************************
+ *HelloJava
+ *Christian Jones
+ *1/27/2021
+ *CMSC 255 003
+ **********************************************************************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+		System.out.println(" Christian Jones ");
+		System.out.println(" HelloJava ");
+		System.out.println(" This lab wil help you get started with Java. You will make sure it is proprerly installed and loaded on your computer and that you can run a program in Java. ");
+		System.out.println(" 1/27/2021 ");
+		System.out.println(" CMSC 255 003 ");
+		System.out.println();
+		}
+}
